a45fa1baf454cc2037d18dd624a35933974611e9
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2020-11-09 13:43:37 -0500

    lista favores

---
+++ lib/classes/favor.dart
@@ -0,0 +1,14 @@
+class Favor {
+  String name;
+  String personAsking;
+  String status;
+  String details;
+  String deliveryPoint;
+
+  Favor(
+      {this.name,
+      this.personAsking,
+      this.status,
+      this.details,
+      this.deliveryPoint});
+}
