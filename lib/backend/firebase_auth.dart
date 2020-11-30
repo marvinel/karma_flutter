0c7483b7bdeb7ee2471f831c4344362f48728bdf
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2020-11-30 12:25:59 -0500

    fixed bugs

--- lib/backend/firebase_auth.dart
+++ lib/backend/firebase_auth.dart
@@ -1,18 +1,21 @@
 import 'dart:async';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:karma/classes/user_model.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 final databaseReference = FirebaseDatabase.instance.reference();
 final FirebaseAuth _auth = FirebaseAuth.instance;
-get _getFirebaseUserReference => databaseReference.child("users");
 
 User currentSignedInUser;
 
-Future<String> signInWithFirebase(email, password) async {
+Future<String> signInWithFirebase(String email, String password) async {
   try {
+    AuthResult result = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
+    print("el Resultado fue");
+    print(result);
     final FirebaseUser user = await _auth.currentUser();
+    print(user);
     final uid = user.uid;
     print("OK " + uid);
 
@@ -29,51 +32,40 @@ Future<String> signInWithFirebase(email, password) async {
       print(nombre);
       currentSignedInUser = User(email: email, name: nombre, uid: id);
     });
-
-    // DatabaseReference ref = FirebaseDatabase.instance.reference();
-    // ref.child('users').child(user.uid).once().then((DataSnapshot snap) {
-    //   var keys = snap.value.keys;
-    //   var data = snap.value;
-    //   for (var key in keys) {
-    //     print(data[key]['nombre']);
-    //     print(data[key]['id']);
-    //     print(data[key]['email']);
-    //   }
-    // });
-
     return Future.value("OK");
   } catch (error) {
     print('ERRORS');
-    print(error.message.toString());
-    print(error.code.toString());
-    return Future.error(error.message.toString());
+    print(error.toString());
+    return Future.value(error.toString());
   }
 }
 
-Future<String> signUpWithFirebase(email, password, name) async {
-  try {} catch (error) {
+Future<String> signUpWithFirebase(
+    String email, String password, String name) async {
+  try {
+    AuthResult result = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    print("el Resultado fue");
+    print(result);
+    final FirebaseUser user = await _auth.currentUser();
+
+    print("token del usuario" + user.uid);
+    final uid = user.uid;
+    print('signUpWithFirebase Ok with uid ' + uid);
+
+    currentSignedInUser = User(email: email, name: name, uid: uid);
+
+    databaseReference.child("users").child(user.uid).set({
+      'id': user.uid,
+      'nombre': currentSignedInUser.name,
+      'email': user.email
+    });
+    return Future.value("OK");
+  } catch (error) {
     print('ERRORS');
-    print(error.message.toString());
-    print(error.code.toString());
-    throw Exception(error.message.toString());
+    print(error.toString());
+    throw Exception(error.toString());
   }
-  final FirebaseUser user = await _auth.currentUser();
-  final uid = user.uid;
-  print('signUpWithFirebase Ok with uid ' + uid);
-
-  currentSignedInUser = User(email: email, name: name, uid: uid);
-
-  databaseReference.child("users").child(user.uid).set({
-    'id': user.uid,
-    'nombre': currentSignedInUser.name,
-    'email': user.email
-  });
-  //await db
-  //.collection('/users')
-  //.document(uid)
-  //.setData(currentSignedInUser.toMap());
-
-  return Future.value("OK");
 }
 
 Future<bool> signOutFirebase() async {
